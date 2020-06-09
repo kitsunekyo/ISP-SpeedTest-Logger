82fb6151f906b79496c8b20d9926982fcb5441b9
Alex Spieslechner <alexander.spieslechner@gmail.com>
2020-06-09 18:17:53 +0200

    automatically accept license and gdpr on server

--- server/src/speedtest/index.js
+++ server/src/speedtest/index.js
@@ -1,13 +1,17 @@
 const speedtestUtil = require("speedtest-net");
 const db = require("./db");
 
+const speedtestOptions = {
+    acceptLicense: true,
+    acceptGdpr: true,
+};
 /**
  * module to run speedtest and persist in lowdb
  */
 const speedtest = (function () {
     async function run() {
         try {
-            const result = await speedtestUtil();
+            const result = await speedtestUtil(speedtestOptions);
             db.create(result);
         } catch (err) {
             console.error(err.message);
