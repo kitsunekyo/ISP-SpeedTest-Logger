7eee4f4d2a159e15a18d6cce6e283acbd1b67293
Alex Spieslechner <alexander.spieslechner@gmail.com>
2021-01-06 11:07:20 +0100

    refactor: remove unused import

--- client/src/Auth/AuthProvider.jsx
+++ client/src/Auth/AuthProvider.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 
