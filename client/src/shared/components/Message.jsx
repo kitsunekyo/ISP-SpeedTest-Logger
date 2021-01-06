15f3cad9021872a8aad0785686e8d6593d051b1c
Alex Spieslechner <alexander.spieslechner@gmail.com>
2021-01-06 12:31:10 +0100

    fix property defaults in message component

--- client/src/shared/components/Message.jsx
+++ client/src/shared/components/Message.jsx
@@ -1,37 +1,37 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { color } from 'shared/utils/style';
+import { color } from "shared/utils/style";
 
 const Wrapper = styled.div`
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	min-height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
 `;
 
 const Title = styled.h1`
-	display: flex;
-	align-items: center;
-	vertical-align: middle;
+    display: flex;
+    align-items: center;
+    vertical-align: middle;
 
-	> svg {
-		margin-left: 0.5rem;
-	}
+    > svg {
+        margin-left: 0.5rem;
+    }
 `;
 const Details = styled.p`
-	color: ${color.textSecondary};
+    color: ${color.textSecondary};
 `;
 
-const Message = ({ title, icon, details }) => {
-	return (
-		<Wrapper>
-			<Title>
-				{title} {icon}
-			</Title>
-			{details && <Details>{details}</Details>}
-		</Wrapper>
-	);
+const Message = ({ title, icon = null, details = null }) => {
+    return (
+        <Wrapper>
+            <Title>
+                {title} {icon}
+            </Title>
+            {details && <Details>{details}</Details>}
+        </Wrapper>
+    );
 };
 
 export default Message;
