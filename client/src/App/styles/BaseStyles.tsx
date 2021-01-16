d4bf828d603c016f1d7c7d9e0671d6ab4d8acbcf
Alex Spieslechner <alexander.spieslechner@gmail.com>
2021-01-16 11:12:51 +0100

    add gray background for body

--- client/src/App/styles/BaseStyles.tsx
+++ client/src/App/styles/BaseStyles.tsx
@@ -6,64 +6,65 @@ const BaseStyles = createGlobalStyle`
     * {
         box-sizing: border-box;
     }
-	html,
-	body,
+    html,
+    body,
     :root {
-		height: 100%;
-		min-height: 100%;
-		margin: 0;
-	}
+        height: 100%;
+        min-height: 100%;
+        margin: 0;
+    }
     #app {
         overflow: hidden;
         min-height: 100%;
     }
-	html {
-		font-size: ${size.base};
-	}
-	body {
-		-webkit-tap-highlight-color: transparent;
-		font-family: ${font.family}, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu,
-			Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-		line-height: 1.5;
-		color: ${color.textPrimary};
-	}
+    html {
+        font-size: ${size.base};
+    }
+    body {
+        -webkit-tap-highlight-color: transparent;
+        font-family: ${font.family}, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu,
+            Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        line-height: 1.5;
+        color: ${color.textPrimary};
+        background-color: ${color.w1};
+    }
 
-	h1,
-	h2,
-	h3,
-	h4,
-	h5 {
-		margin: 0;
-		font-weight: 500;
-		margin-top: 1rem;
-		margin-bottom: 1rem;
-	}
+    h1,
+    h2,
+    h3,
+    h4,
+    h5 {
+        margin: 0;
+        font-weight: 500;
+        margin-top: 1rem;
+        margin-bottom: 1rem;
+    }
 
-	h1 {
-		font-size: 1.5rem;
-	}
+    h1 {
+        font-size: 1.5rem;
+    }
 
-	h2 {
-		font-size: 1.25rem;
-		font-weight: normal;
-	}
+    h2 {
+        font-size: 1.25rem;
+        font-weight: normal;
+    }
 
-	a,
-	a:visited,
-	a:focus {
-		color: ${color.primary};
+    a,
+    a:visited,
+    a:focus {
+        color: ${color.primary};
 
-		&:hover {
-			text-decoration: none;
-		}
-	}
+        &:hover {
+            text-decoration: none;
+        }
+    }
 
-	/* button {
-		background: none;
-		border-width: initial;
-		border-style: none;
-		border-color: initial;
-		border-image: initial;
+    /* button {
+        background: none;
+        border-width: initial;
+        border-style: none;
+        border-color: initial;
+        border-image: initial;
         border-radius: ${size.radius};
         height: 35px;
         padding: 0 1rem;
@@ -77,7 +78,7 @@ const BaseStyles = createGlobalStyle`
         &:hover {
             background: ${color.w1}
         }
-	} */
+    } */
 
 `;
 
