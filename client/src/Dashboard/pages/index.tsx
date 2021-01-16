320ac1631401af68ff921d2617c103975fdc90ba
Alex Spieslechner <alexander.spieslechner@gmail.com>
2021-01-16 12:09:52 +0100

    move dashboard container into page

--- client/src/Dashboard/pages/index.tsx
+++ client/src/Dashboard/pages/index.tsx
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { RefreshCcw } from 'react-feather';
 
-import Dashboard from 'Dashboard/components/Dashboard';
-import { ResultsProvider } from 'Dashboard/ResultsContext';
 import { SidebarLayout } from 'shared/components/SidebarLayout';
+import { PageTitle, SectionTitle } from 'shared/components/styles';
+import Button from 'shared/components/Button';
+import { resultsContext, ResultsProvider } from 'Dashboard/ResultsContext';
+import Speedtest from 'Dashboard/components/Speedtest';
+import ScheduleSelector from 'Dashboard/components/ScheduleSelector';
+import AveragePerformance from 'Dashboard/components/AveragePerformance';
+import TestHistory from 'Dashboard/components/TestHistory';
 
 const DashboardPage = () => {
+    const { loadResults } = useContext(resultsContext);
+
+    const handleReload = () => {
+        loadResults();
+    };
+
+    // if (state.isLoading) return <Message title="Loading speedtest results" icon={<Loader />} />;
+    // if (state.error)
+    //     return <Message title="Error loading speedtest results" icon={<AlertOctagon />} />;
+
     return (
         <ResultsProvider>
             <SidebarLayout>
-                <Dashboard></Dashboard>
+                <PageTitle>Network Quality Dashboard</PageTitle>
+                <Button
+                    onClick={handleReload}
+                    icon={<RefreshCcw size={14} />}
+                    style={{ float: 'left', marginRight: '.5rem' }}
+                />
+                <Speedtest />
+
+                <SectionTitle>Automated Speedtest</SectionTitle>
+                <ScheduleSelector />
+
+                <SectionTitle>Average Performance</SectionTitle>
+                <AveragePerformance />
+
+                <SectionTitle>Test History</SectionTitle>
+                <TestHistory />
             </SidebarLayout>
         </ResultsProvider>
     );
